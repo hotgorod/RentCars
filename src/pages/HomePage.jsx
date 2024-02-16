c104883e4e7da0bb40be7536b0fa7d0d2dcf920b
hotgorod <hotgorod@gmail.com>
2024-02-16 15:38:33 +0000

    base changed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
+import video from "../assets/video.mp4";
+
 const HomePage = () => {
   return (
     <div>
+      <video src={video} autoPlay loop muted />
       Welcome page!
     </div>
   );
-}
+};
 
 export default HomePage;
